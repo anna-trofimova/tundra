ac781c348b70e503859c763bd666e37bb35bb7d4
Anna Trogimova <cobrana1000@gmail.com>
2019-12-26 19:07:45 +0300

    about page, added logo to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <div>
-              <ul>
-          <li><Link to='/'>About</Link></li>
-          <li><Link to='/brands'>Brands</Link></li>
-          <li><Link to='/culture'>Culture</Link></li>
-          <li><Link to='contacts'>Contacts</Link></li>
-        </ul>
+    <div className='navbar'> 
+     <img src={'/logo-tundra.png'} alt='logo' width='80px'/>
+     <ul>
+        <li><Link to='/'>About</Link></li>
+        <li><Link to='/brands'>Brands</Link></li>
+        <li><Link to='/culture'>Culture</Link></li>
+        <li><Link to='contacts'>Contacts</Link></li>
+     </ul>
     </div>
   )
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class About extends Component {
   render() {
     return (
-      <div>
+      <div className='about'>
+        <img src={'/images/tundra-baner.jpg'} alt={'jfkd'}/>
         <h1>About</h1>
       </div>
     )
